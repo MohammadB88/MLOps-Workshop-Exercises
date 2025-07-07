fbb3243a323311fcd9788de86e5523ff6438c51a
Mohammad Bahmani <bahmani.m.n@gmail.com>
2025-07-07 07:57:20 +0200

    bike-sharing - add python files for pipeline prozess

--- workshop_materials/bike_demand_forecasting_pipeline/03_model_deployment.py
+++ workshop_materials/bike_demand_forecasting_pipeline/03_model_deployment.py
@@ -1,7 +1,25 @@
+# Import necessary modules
+import os
+import joblib
 
+import requests
+import json
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
+import pandas as pd
+import numpy as np
+
+import mlflow
+import mlflow.sklearn
+from mlflow.tracking import MlflowClient
 
 
 ##################################################################################################
+# Select and Load the trained model
+# The Model is trained in the previous step and saved in the MLflow tracking server.
 
 
 
